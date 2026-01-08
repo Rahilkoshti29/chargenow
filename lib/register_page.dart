a9098d57aca49f034c561e1cb9e1a81c5e10d9ed
rahil koshti <rahilkoshti296@gmail.com>
2026-01-08 20:32:42 +0530

    8-1 8:30

--- lib/register_page.dart
+++ lib/register_page.dart
@@ -17,6 +17,7 @@ class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: const Text("Register"),
         centerTitle: true,
