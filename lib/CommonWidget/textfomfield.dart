359ffccd6605da1257a54a653164069011cacbe4
rahil koshti <rahilkoshti296@gmail.com>
2026-01-08 20:14:39 +0530

    8-1 6:26

---
+++ lib/CommonWidget/textfomfield.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class CommonTextFormField extends StatelessWidget {
+  final TextEditingController controller;
+  final String hintText;
+  final IconData prefixIcon;
+  final bool obscureText;
+  final Widget? suffixIcon;
+  final String? Function(String?)? validator;
+  final TextInputType keyboardType;
+
+  const CommonTextFormField({
+    super.key,
+    required this.controller,
+    required this.hintText,
+    required this.prefixIcon,
+    this.obscureText = false,
+    this.suffixIcon,
+    this.validator,
+    this.keyboardType = TextInputType.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      obscureText: obscureText,
+      validator: validator,
+      keyboardType: keyboardType,
+      decoration: InputDecoration(
+        hintText: hintText,
+        prefixIcon: Icon(prefixIcon),
+        suffixIcon: suffixIcon,
+        filled: true,
+        fillColor: Colors.grey.shade100,
+        contentPadding:
+        const EdgeInsets.symmetric(vertical: 16, horizontal: 20),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30),
+          borderSide: BorderSide.none,
+        ),
+      ),
+    );
+  }
+}
--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,20 +1,210 @@
+import 'package:chargenow/CommonWidget/textfomfield.dart';
+import 'package:chargenow/register_page.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
 
   @override
-  State<LoginPage> createState() => _LoginPageState();
+  State<LoginScreen> createState() => _LoginScreenState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  bool hidePassword = true;
+  bool isLoading = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Login page"),),
-      body: Center(
-        child: Text("Login page"),
-      ),
+      backgroundColor: Colors.white,
+      resizeToAvoidBottomInset: true,
+      body: isLoading
+          ?  Center(
+              child: CircularProgressIndicator(color: Color(0xff2ecc71)),
+            )
+          : Column(
+              children: [
+                Expanded(
+                  flex: 4,
+                  child: Container(
+                    width: double.infinity,
+                    decoration:  BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [Color(0xff2ecc71), Color(0xff27ae60)],
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                      ),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.ev_station, color: Colors.white, size: 80),
+                        SizedBox(height: 10),
+                        Text(
+                          "ChargeNow",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 28,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+
+                Expanded(
+                  flex: 6,
+                  child: Transform.translate(
+                    offset:  Offset(
+                      0,
+                      -30,
+                    ),
+                    child: ClipRRect(
+                      borderRadius:  BorderRadius.vertical(
+                        top: Radius.circular(35),
+                      ),
+                      child: Container(
+                        color: Colors.white,
+                        padding:  EdgeInsets.fromLTRB(25, 40, 25, 25),
+                        child: SingleChildScrollView(
+                          child: Form(
+                            key: _formKey,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                 Center(
+                                  child: Text(
+                                    "Welcome Back!",
+                                    style: TextStyle(
+                                      fontSize: 28,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+
+                                 SizedBox(height: 30),
+
+                                CommonTextFormField(
+                                  controller: emailController,
+                                  hintText: "Email",
+                                  prefixIcon: Icons.email_outlined,
+                                  validator: (value) {
+                                    if (value!.isEmpty)
+                                      return "Email is required";
+                                    if (!RegExp(
+                                      r'^[^@]+@[^@]+\.[^@]+',
+                                    ).hasMatch(value)) {
+                                      return "Enter valid email";
+                                    }
+                                    return null;
+                                  },
+                                ),
+
+                                 SizedBox(height: 20),
+
+                                CommonTextFormField(
+                                  controller: passwordController,
+                                  hintText: "Password",
+                                  prefixIcon: Icons.lock_outline,
+                                  obscureText: hidePassword,
+                                  suffixIcon: IconButton(
+                                    icon: Icon(
+                                      hidePassword
+                                          ? Icons.visibility_off
+                                          : Icons.visibility,
+                                    ),
+                                    onPressed: () {
+                                      setState(() {
+                                        hidePassword = !hidePassword;
+                                      });
+                                    },
+                                  ),
+                                  validator: (value) {
+                                    if (value!.isEmpty)
+                                      return "Password is required";
+                                    if (value.length < 6)
+                                      return "Minimum 6 characters";
+                                    return null;
+                                  },
+                                ),
+
+                                 SizedBox(height: 30),
+
+                                SizedBox(
+                                  width: double.infinity,
+                                  height: 55,
+                                  child: ElevatedButton(
+                                    style: ElevatedButton.styleFrom(
+                                      backgroundColor:  Color(0xff2ecc71),
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(30),
+                                      ),
+                                    ),
+                                    onPressed: () {
+                                      if (_formKey.currentState!.validate()) {
+                                        ScaffoldMessenger.of(
+                                          context,
+                                        ).showSnackBar(
+                                           SnackBar(
+                                            content: Text("Login Successful"),
+                                          ),
+                                        );
+                                      }
+                                    },
+                                    child: Text(
+                                      "Login",
+                                      style: TextStyle(
+                                        fontSize: 25,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+
+                                 SizedBox(height: 25),
+
+                                Center(
+                                  child: GestureDetector(
+                                    onTap: () {
+                                      Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) => RegisterPage()));
+                                    },
+                                    child:  Text.rich(
+                                      TextSpan(
+                                        text: "Don't have an account? ",
+                                        style: TextStyle(color: Colors.black),
+                                        children: [
+                                          TextSpan(
+                                            text: "Sign Up",
+                                            style: TextStyle(
+                                              color: Color(0xff2ecc71),
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
     );
   }
 }
